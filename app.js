5bcab06d90b856c3c68e34cc4a13ffe1fa11a96d
JaundiceJack <mcneilanjames@gmail.com>
2019-10-21 15:02:57 -0600

    pug,views, and static set up

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
-const test = 'five';
+const port = 8080;
+
+const app = express();
+
+// Tell express to use pug and point it to the views(pages) and public(css/js) folders
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.send("Hello")
+})
+
+app.listen(port, () => {
+	console.log("Express started on port:"+port.toString());
+})
