ff7ed308f9814a7de99485d4c60e49ba91efd1b9
JaundiceJack <mcneilanjames@gmail.com>
2020-08-02 16:54:19 -0600

    makin a home page

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+// Load in dependancies
 const express = require('express');
+const path = require('path');
 
-const port = 8080;
-
+// Instance the app/page and show it where to find the css/html/js
 const app = express();
-
-// Tell express to use pug and point it to the views(pages) and public(css/js) folders
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Display the home page when requested
 app.get('/', (req, res) => {
-	res.send("Hello")
-})
+	res.render('index', {});
+});
 
+// Route to Dino-Data
+let dinodata = require('./routes/dinodata');
+app.use('/dinodata', dinodata);
+
+// Start the app on port 80
+const port = 8080;
 app.listen(port, () => {
 	console.log("Express started on port:"+port.toString());
 })
