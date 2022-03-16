0d733fd661fa2d2352ef5d3a5271a2ebea843b31
Jame McNeilan <jamesmcneilan@protonmail.com>
2022-03-16 17:46:09 -0600

    Removed dinodata, added libercord and web of wares

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-// Load in dependancies
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+//const connect = require('./db/connection.js');
 const path = require('path');
+const { notFound, errorFound } = require('./middleware/errorMW.js')
+dotenv.config();
 
-// Instance the app/page and show it where to find the css/html/js
+// Instance the app server and use the internal body parser
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-// Display the home page when requested
-app.get('/', (req, res) => {
-	res.render('index', {});
-});
+// Instance a mongoDB connection
+//connect();
 
-// Route to Dino-Data
-let dinodata = require('./routes/dinodata');
-app.use('/dinodata', dinodata);
+// Define Routes
+app.get('/', (req, res) => { res.render('index', {}); });
+//app.use('/api/users', require('./routes/paths/users.js'));
+// Define a route to ensure the server is functioning
+app.get('/ping', (req, res) => { return res.send('pong'); });
 
-// Start the app on port 80
-const port = 8080;
-app.listen(port, () => {
-	console.log("Express started on port:"+port);
-})
+// Error handling middleware
+app.use(notFound);
+app.use(errorFound);
+
+// Start the server on the assigned port
+const port = process.env.PORT || 5000;
+app.listen(port, () => { console.log(`Server started on port ${port}`); });
